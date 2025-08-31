eb01ca05d54c7469d4fb1ebb6cddfa648639de11
TranSang231 <dinhsang2313@gmail.com>
2025-08-31 15:21:22 +0700

    practice view function

--- lab3/page/urls.py
+++ lab3/page/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),         # URL /
     path('about/', views.about, name='about'),   # URL /about/
+    path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
+    path('products/', views.products, name='products'),
+    path('blog/', views.blog, name='blog'),
+    path('faq/', views.faq, name='faq'),
 ]
--- lab3/page/views.py
+++ lab3/page/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Đây là trang chủ")
+
+def about(request):
+    return HttpResponse("Đây là trang giới thiệu")
+
+def contact(request):
+    return HttpResponse("Đây là trang liên hệ")
+
+def services(request):
+    return HttpResponse("Đây là trang dịch vụ")
+
+def products(request):
+    return HttpResponse("Đây là trang sản phẩm")
+
+def blog(request):
+    return HttpResponse("Đây là trang blog")
+
+def faq(request):
+    return HttpResponse("Đây là trang câu hỏi thường gặp (FAQ)")
